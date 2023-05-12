436fec7543c0d19762b0b5906fd35aa89988f17d
alexmunozbravo <alex.munoz.bravo@estudiantat.upc.edu>
2023-05-12 13:30:33 +0200

    Create cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This repository will contain the code and documentation created for the Individual Project coursed in ITU as part of my final Bachelor Thesis.',
+    author='Alex Muñoz Bravo',
+    license='MIT',
+)
